0a6a38c0e2c1b0f890dfb7d86aaaa95f42a5b608
xc8pt <brolike636@gmail.com>
2025-11-20 12:47:34 +0300

    Laba3

--- app/src/main/java/com/abdulin/pixletter/MainActivity.kt
+++ app/src/main/java/com/abdulin/pixletter/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +32,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PixLetterTheme {
-                GreetingText(message = "Royal Decree!\n Dear Rinat" +
+                GreetingImage(
+                    message = "Royal Decree!\n Dear Rinat" +
                         "\nHis Majesty the King\nInvites you to a great feast!" +
                         "\nCome to the castle before sunset, " +
                         "\notherwise, you risk losing your head!", from = "From King Arthur",
@@ -36,27 +47,71 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
+            .border(
+                width = 2.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .fillMaxSize()
+            .padding(top = 10.dp)
         ) {
-        Text(
-            text = message,
-            fontSize = 20.sp,
-            lineHeight = 40.sp,
-            textAlign = TextAlign.Center
+        InvitationTitle()
+//        Text(
+//            text = message,
+//            fontSize = 20.sp,
+//            lineHeight = 40.sp,
+//            textAlign = TextAlign.Center,
+//            color = Color.White
+//        )
+//        Text(
+//            text = from,
+//            fontSize = 36.sp,
+//            color = Color.White,
+//            modifier = Modifier
+//                .padding(16.dp)
+//                .align(alignment = Alignment.End)
+//        )
+    }
+}
+@Composable
+fun InvitationTitle(modifier: Modifier = Modifier){
+    Text(
+        stringResource(R.string.invitation_title),
+        fontSize = 28.sp,
+        color = Color.Yellow,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp)
+    )
+}
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.pixelcasstle)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            alpha = 0.8F,
+            modifier = Modifier.fillMaxSize()
         )
-        Text(
-            text = from,
-            fontSize = 36.sp
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RoyalInvitationPreview() {
     PixLetterTheme {
-        GreetingText(message = "Royal Decree!\n Dear Rinat" +
-                "\nHis Majesty the King\nInvites you to a great feast!" +
-                "\nCome to the castle before sunset, " +
-                "\notherwise, you risk losing your head!", from = "From King Arthur")
+//        GreetingImage(
+//            message = stringResource(R.string.king_massage_text),
+//            from = stringResource(R.string.from_king_arthur)
+//        )
     }
 }
